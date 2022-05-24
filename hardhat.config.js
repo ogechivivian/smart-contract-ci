2667f8f8293a045be2e6819316ee9e905ab8d429
Ogechi Vivian <ogechivivian@Ogechis-MacBook-Pro.local>
2022-05-24 23:25:44 +0100

    deploying smart-contract with github Actions

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    kovan: {
+      url: 'https://kovan.poa.network/',
+      accounts: ['e10d2bd3ec8630f167c455c2ca565a62112b1737331264dcefe9005339bb8bee'],
+    },
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+const main = async () => {
+    const ContractFactory = await hre.ethers.getContractFactory('Greeter') // the file name under 'contracts' folder, without '.sol'
+    const Contract = await ContractFactory.deploy("hello World") // the constructor params
+    await Contract.deployed()
+    console.log("Contract deployed to:", Contract.address)
+
+    // // You can test the function.
+    // let txn = await nftContract.functionName()
+    // // Wait for it to be mined.
+    // await txn.wait()
+    // console.log("function invoked!")
+}
+
+const runMain = async () => {
+    try {
+        await main()
+        process.exit(0) // emit the exit event that ends all tasks immediately even if there still are asynchronous operations not been done. The shell that executed node should see the exit code as 0.
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+runMain()
